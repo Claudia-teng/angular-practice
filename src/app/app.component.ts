e30bb3ae45d0c72f45fa323c99c885fa3fcb4312
Claudia Teng <sydeng102@gmail.com>
2021-09-04 14:07:54 +0800

    add RecipeService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-practice';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
